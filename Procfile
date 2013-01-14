ea53f837a58d8528e562daab288f391ed36df030
Mateusz Korszun <mkorszun@gmail.com>
2013-01-14 16:37:15 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:    java -cp target/example-1.0-SNAPSHOT/WEB-INF/classes:target/example-1.0-SNAPSHOT/WEB-INF/lib/* com.cloudcontrolled.java.gwt.server.GreetingServiceEmbedded
---
+++ src/main/java/com/cloudcontrolled/java/gwt/server/GreetingServiceEmbedded.java
@@ -0,0 +1,27 @@
+package com.cloudcontrolled.java.gwt.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class GreetingServiceEmbedded {
+
+    public static void main(String[] args) throws Throwable {
+
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+
+        // Create a handler for processing our GWT app
+        WebAppContext handler = new WebAppContext();
+        handler.setContextPath("/");
+        handler.setWar("./target/example-1.0-SNAPSHOT.war");
+
+        // Add it to the server
+        server.setHandler(handler);
+
+        // And start it up
+        server.start();
+        server.join();
+    }
+}
